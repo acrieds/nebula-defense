77da41a75c0cb322da551479daad14cc151dc50c
zion <129014249+acrieds@users.noreply.github.com>
2023-05-04 11:53:06 +1000

    Initial files for MakeCode project

---
+++ images.g.ts
@@ -0,0 +1,44 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "image1":
+            case "spacePinkShip":return img`
+. . . . . . . c d . . . . . . . 
+. . . . . . . c d . . . . . . . 
+. . . . . . . c d . . . . . . . 
+. . . . . . . c b . . . . . . . 
+. . . . . . . f f . . . . . . . 
+. . . . . . . c 3 . . . . . . . 
+. . . . . . . f f . . . . . . . 
+. . . . . . . b 3 . . . . . . . 
+. . . . . . b b 1 a . . . . . . 
+. . . . . . b 3 1 a . . . . . . 
+. . . . . c c c a a a . . . . . 
+. . . . b b 3 3 3 1 a a . . . . 
+. . b f f f c c a a f f a a . . 
+. b 1 1 c c a 3 3 3 3 1 3 a a . 
+b 1 1 c c c c 3 3 3 3 1 3 3 a a 
+b 1 1 c 3 3 c c 3 3 3 3 1 3 a a 
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+    helpers._registerFactory("song", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
